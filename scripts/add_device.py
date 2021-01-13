a72bc8d7919036ba5cea53aa7a470088b5f85a1e
nic0lasfara <nicolas.farabegoli@gmail.com>
2021-01-13 22:24:19 +0100

    Finish scripts

---
+++ scripts/add_device.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021 Nicolas Farabegoli <nicolas.farabegoli@gmail.com> & Martina Cavallucci <martina.cavallucci11@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import asyncio
+import aiohttp
+
+# TODO("Converti i parametri affinche' siano validi in produzione")
+credential={
+        "email": "jonny@ivy.com",
+        "password": "jonny"
+}
+
+async def main():
+    base_url="localhost/api"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=f"http://{base_url}/users/login", json=credential) as resp:
+            res = await resp.json()
+            token = res['token']
+        headers={'Authorization': f"Bearer {token}"}
+        async with session.post(url=f"http://{base_url}/users/device", headers=headers) as resp:
+            device_id = await resp.json()
+            print(device_id)
+            if resp.status == 200:
+                print("Device added successfully")
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ scripts/add_status.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021 Nicolas Farabegoli <nicolas.farabegoli@gmail.com> & Martina Cavallucci <martina.cavallucci11@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import asyncio
+import json
+from asyncio_mqtt import Client
+
+# TODO("Converti i parametri affinche' siano validi")
+payload = {
+    "deviceId":"2a19abbcdddcb24596785bb5",
+    "message": "In 5 days the feed will run out!"
+}
+
+async def main():
+    async with Client("localhost") as client:
+        msg = json.dumps(payload)
+        await client.publish('/pfeeder/status', msg, qos=1)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
