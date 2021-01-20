d41c01a201e1d82c19fd3714f57e210518aa49ad
Martina <martina.cavallucci@studio.unibo.it>
2021-01-20 17:43:44 +0100

    Fix typo

--- frontend-angular/src/app/_component/ration/showration/showration.component.ts
+++ frontend-angular/src/app/_component/ration/showration/showration.component.ts
@@ -31,7 +31,6 @@ export class ShowrationComponent implements OnInit {
   getRation() {
     this.dataService.getRation().subscribe((rations1: Ration[]) => {
         this.rations = rations1;
-        this.rations.forEach( x=> x.time = x.time.)
       },
       (error => {
         console.error('error caught in component');
